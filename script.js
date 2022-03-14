9273b9ef6cb2ecf026761cf446577527334fe8c6
tammatYO <95525800+tammatYO@users.noreply.github.com>
2022-03-14 15:33:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-z
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+title = "idk";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 5000;
+rollback = 50;
+fullPrice = 10000;
+adaptive = true;
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log(screenPrice + " " + "рублей");
+console.log(fullPrice + " " + "рублей");
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback/100));
+
+//alert ("Hello world!");
+console.log("Hello world!");
