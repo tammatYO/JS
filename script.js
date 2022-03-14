57ebfd8eeda953be26742bd6ce0468eef8c3ed3d
tammatYO <95525800+tammatYO@users.noreply.github.com>
2022-03-15 00:39:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-z
+const num = '266219';
+let mas = 1;
+
+num.toString().split('').forEach(elem => mas *= elem);
+console.log('получилось' + " " + mas);
+
+const ms = mas ** 3;
+console.log(ms)
+console.log(ms.toString().substr(0,2))
