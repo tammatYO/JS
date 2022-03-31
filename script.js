c6823d676734307085702fee15ed7dae3660d71a
tammatYO <95525800+tammatYO@users.noreply.github.com>
2022-03-31 10:35:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,41 @@
 'use strict';
 
-let book = document.querySelectorAll('.book')
-let title = document.querySelectorAll('.book > h2')
-let bg = document.querySelector('body')
-let adv = document.querySelector('.adv')
+const btn = document.getElementById('btn')
+let square = document.getElementById('square')
+let input = document.getElementById('text')
+let e_btn = document.getElementById('e_btn')
+let range = document.getElementById('range')
+let circle = document.getElementById('circle')
+let span = document.getElementById('range-span')
 
-book[1].after(book[0])
-book[4].after(book[3])
-book[4].after(book[2])
-book[3].after(book[2])
-book[2].before(book[5])
 
-bg.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)'
+const color = function () {
+        square.style.backgroundColor = input.value  
+}
 
-title[4].textContent = "Книга 3. this и Прототипы Объектов"
-title[4].style.color = "darkkhaki"
+btn.addEventListener('click', () => {
+    color()
+})
 
-document.querySelector('.adv').remove()
+const invisible = function () {
+    e_btn.style.display = 'none'
+}
 
-let chapter = document.querySelectorAll('.book > ul')
-console.dir(chapter)
+e_btn.addEventListener('click', () => {
+    invisible()
+})
 
-let chapter2 = chapter[1].querySelectorAll('li')
-console.log(chapter2);
+const target = function (event) {
+    span.textContent = event.target.value
+}
 
-chapter2[8].after(chapter2[2])
-chapter2[6].after(chapter2[4])
-chapter2[6].after(chapter2[5])
-chapter2[5].before(chapter2[8])
-chapter2[5].before(chapter2[4])
-chapter2[6].after(chapter2[4])
-chapter2[2].before(chapter2[9])
+range.addEventListener('change', target)
 
-let chapter5 = chapter[4].querySelectorAll('li')
-console.log(chapter5);
+const hw = function () {
+    circle.style.height = range.value + '%'
+    circle.style.width = range.value + '%'
+}
 
-chapter5[2].before(chapter5[9])
-chapter5[2].before(chapter5[3])
-chapter5[2].before(chapter5[4])
-chapter5[5].before(chapter5[6])
-chapter5[5].before(chapter5[7])
+range.addEventListener('input', hw)
 
-let li = document.createElement('li')
-li.innerHTML = '<li>Глава 8: За пределами ES6</li>';
-chapter[5].appendChild(li)
-let chapter6 = chapter[5].querySelectorAll('li')
-console.log(chapter6);
-chapter6[10].after(chapter6[9])
-
-// https://codepen.io/goslyan/pen/ExoXXbe
+console.dir(circle)
